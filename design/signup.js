bdfcf563103b16b5d6a072e07077f917ed680831
yodit93 <yoditabebe9391@gmail.com>
2023-09-14 19:40:55 +0300

    Handle validation error

--- design/signup.js
+++ design/signup.js
@@ -1,9 +1,22 @@
 const signupForm = document.querySelector('.form');
+const emailInput = document.querySelector('.email');
 
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const emailInput = document.querySelector('#email');
     const userEmail = emailInput.value;
-    localStorage.setItem("userEmail", userEmail);
-    window.location.href = "success.html";
+    const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if(!pattern.test(userEmail)) {
+        emailInput.classList.add('errorstate');
+        document.querySelector('.error').classList.add('show');
+        return;
+    }
+    else {
+        localStorage.setItem("userEmail", userEmail);
+        window.location.href = "success.html";
+    }
 });
+
+emailInput.addEventListener('input', () => {
+    emailInput.classList.remove('errorstate');
+    document.querySelector('.error').classList.remove('show');
+})
